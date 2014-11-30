9d5a0eac1c3e76ba5607e9e048ccf32c5b3c5ec4
Pontus Viking <pontus.viking@meradan.se>
2014-11-30 16:37:12 +0100

    Changed create player form to use the UserController

--- create_player.php
+++ create_player.php
@@ -1,47 +1,47 @@
-<?php $admin=($_SESSION['level']==3)?(TRUE):(FALSE);
-if ($admin) {
-myconnect();
-mysql_select_db("skynet");
+<?php
+$userController = new UserController();
+$user = $userController->getCurrentUser();
+if ($user->isAdmin()) {
 ?>
 <br><br>
 <form method="post" action="player.php?what=create">
 <table width="50%"  border="0" cellspacing="1" cellpadding="1">
-	<tr>
-		<td>Forname</td>
-		<td><input type="text" name="forname"></td>
-	</tr>
-	<tr>
-		<td>Nickname</td>
-		<td><input type="text" name="nickname"></td>
-	</tr>
-	<tr>
-		<td>Use nickname instead of real name</td>
-		<td><input type="radio" name="use_nickname" value="1">Yes 
-		    <input type="radio" name="use_nickname" value="0">No 
-		</td>
-	</tr>
-	<tr>
-		<td>Lastname</td>
-		<td><input type="text" name="lastname"></td>
-	</tr>
-	<tr>
-		<td>emailadress</td>
-		<td><input type="text" name="emailadress"></td>
-	</tr>
-	<tr>
-		<td>Platoon</td>
-		<td><input type="text" name="platoon_id" ></td>
-	</tr>
-	<tr>
-		<td>password</td>
-		<td><input type="password" name="password"></td>
-	</tr>
-	<tr>
-		<td colspan="2"><input type="submit" value="Create Player"/></td>
+  <tr>
+    <td>Forname</td>
+    <td><input type="text" name="forname"></td>
+  </tr>
+  <tr>
+    <td>Nickname</td>
+    <td><input type="text" name="nickname"></td>
+  </tr>
+  <tr>
+    <td>Use nickname instead of real name</td>
+    <td><input type="radio" name="use_nickname" value="1">Yes
+        <input type="radio" name="use_nickname" value="0">No
+    </td>
+  </tr>
+  <tr>
+    <td>Lastname</td>
+    <td><input type="text" name="lastname"></td>
+  </tr>
+  <tr>
+    <td>emailadress</td>
+    <td><input type="text" name="emailadress"></td>
+  </tr>
+  <tr>
+    <td>Platoon</td>
+    <td><input type="text" name="platoon_id" ></td>
+  </tr>
+  <tr>
+    <td>password</td>
+    <td><input type="password" name="password"></td>
+  </tr>
+  <tr>
+    <td colspan="2"><input type="submit" value="Create Player"/></td>
 </tr>
 </table>
 </form>
-<?php } 
+<?php }
 else {
 include("not_allowed.php");
 }?>
