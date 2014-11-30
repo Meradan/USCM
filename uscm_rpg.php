ac46bb7bdd5b9d437e8d9640db9ee03977cb4d0f
Pontus Viking <pontus.viking@meradan.se>
2014-11-30 15:40:00 +0100

    Fixed broken link

--- uscm_rpg.php
+++ uscm_rpg.php
@@ -2,32 +2,32 @@
 
 <div class="title">Swedish Info</div>
 
-Rollspelskampanjen som den här sidan handlar om utspelar sig i samma miljö som Alien-filmerna. Spelarnas karaktärer är 
-soldater i framtidens motsvarighet till den amerikanska marinkåren och tillhör plutonen Brixton's Fightin' Fives. 
+Rollspelskampanjen som den här sidan handlar om utspelar sig i samma miljö som Alien-filmerna. Spelarnas karaktärer är
+soldater i framtidens motsvarighet till den amerikanska marinkåren och tillhör plutonen Brixton's Fightin' Fives.
 Stämningen i kampanjen inriktas i första hand på action, spänning och taktisk planering.<BR><BR>
 
-Kampanjen har ingen fast spelargrupp. Den är istället organiserad på ett sätt som låter många fler spelare vara med i den. 
-Varje uppdrag är ett fristående äventyr. Alla spelare är med på en maillista och när en spelledare har gjort ett nytt äventyr 
-skickas ett "announcement" för detta ut på maillistan. Detta innehåller tid och plats, antal spelare samt en kort beskrivning 
-av uppdraget. Sedan är det först till kvarn. De första spelarna som anmäler sig får en mer detaljerad briefing som bekräftelse 
-på att dom har kommit med på uppdraget.<BR> Det gör att det blir variation i spelgruppen från gång till gång och det går bra att 
-vara med i kampanjen även om man inte kan spela så ofta. I de fall där alldeles för många 
+Kampanjen har ingen fast spelargrupp. Den är istället organiserad på ett sätt som låter många fler spelare vara med i den.
+Varje uppdrag är ett fristående äventyr. Alla spelare är med på en maillista och när en spelledare har gjort ett nytt äventyr
+skickas ett "announcement" för detta ut på maillistan. Detta innehåller tid och plats, antal spelare samt en kort beskrivning
+av uppdraget. Sedan är det först till kvarn. De första spelarna som anmäler sig får en mer detaljerad briefing som bekräftelse
+på att dom har kommit med på uppdraget.<BR> Det gör att det blir variation i spelgruppen från gång till gång och det går bra att
+vara med i kampanjen även om man inte kan spela så ofta. I de fall där alldeles för många
 har velat vara med på ett uppdrag har det spelats två gånger med olika spelargrupper.<BR><BR>
 
-Första uppdraget i kampanjen spelades i november år 2000. Därefter har det blivit många fler. Alla uppdrag finns beskrivna under "missions" med den 
-briefing-text som spelarna fick ut innan uppdraget och med sammanfattningen som skrivits efteråt av spelledaren och ibland av 
+Första uppdraget i kampanjen spelades i november år 2000. Därefter har det blivit många fler. Alla uppdrag finns beskrivna under "missions" med den
+briefing-text som spelarna fick ut innan uppdraget och med sammanfattningen som skrivits efteråt av spelledaren och ibland av
 någon spelare.<BR><BR>
 
-USCM-kampanjen har legat i dvala ett antal år även om den har dykt upp som arrangemang på spelkonventet LinCon ibland. 
+USCM-kampanjen har legat i dvala ett antal år även om den har dykt upp som arrangemang på spelkonventet LinCon ibland.
 Men den är inte död, den finns kvar någonstans där ute i rymdens kalla mörker och kanske väntar på att få döda just din karaktär..<BR><BR>
 
 <br/><img src="images/linje.jpg" width="449" height="1" align="middle"><br/>
 
 <div class="title">English Info</div>
-This is the homepage for a roleplaying campaign based on the Alien movies. The players are members of the 5th platoon 
+This is the homepage for a roleplaying campaign based on the Alien movies. The players are members of the 5th platoon
 "Brixton's Fightin' Fives" in the 4th US Colonial Marine brigade.<BR><BR>
 
-The goal of this campaign is to mix the atmosphere of the Alien movies with a military-style RPG campaign where 
+The goal of this campaign is to mix the atmosphere of the Alien movies with a military-style RPG campaign where
 we focus on action and tactical planning.<BR><BR>
 
 All roleplaying is done in the "traditional" way with paper and dice - there is no online roleplaying. Because of that only
@@ -49,6 +49,4 @@ swedish players can join the campaign and most content on this webpage is in swe
 <div class="title">Creators</div>
 Campaign &amp; Content: Oskar (<A HREF="mailto:oskar.grindemyr@gmail.com?subject=USCM">oskar.grindemyr@gmail.com</A>)<BR>
 Web Production: Totte (<A HREF="mailto:tottemans@hotmail.com?subject=USCM">tottemans@hotmail.com</A>)<BR>
-Database Production: Pontus (<A HREF="mailto:meradan@gmail.com?subject=USCM">meradan@gmail.com</A>)<BR>
-
-<BR><CENTER><A HREF="http://totte.digitalrice.com/" TARGET="_blank"><IMG SRC="oldweb/tottetk.gif" BORDER="0" WIDTH="95" HEIGHT="43" ALT="www.totte.tk"></A></CENTER>
+Database Production: Pontus (<A HREF="mailto:meradan+uscm@gmail.com?subject=USCM">meradan@gmail.com</A>)<BR>
