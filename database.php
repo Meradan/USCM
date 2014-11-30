328a7d47ecb3d0fe16df859f4cd64ec3d558bf30
Pontus Viking <pontus.viking@meradan.se>
2014-11-30 16:34:28 +0100

    Commented out debug information

--- database.php
+++ database.php
@@ -1,27 +1,27 @@
-<?
-session_start();
-include("functions.php");
-myconnect();
-mysql_select_db("skynet");
+<?php
+// session_start();
+// include("functions.php");
+// myconnect();
+// mysql_select_db("skynet");
 
-if ($_GET['what']=="cert") {
-	mysql_query("INSERT INTO {$_SESSION['table_prefix']}certificate_requirements SET certificate_id='{$_POST['certname']}',
-			req_item='{$_POST['req1']}',value='{$_POST['value1']}',table_name='{$_POST['from1']}'");
-	if ($_POST['value2']!="") {
-	mysql_query("INSERT INTO {$_SESSION['table_prefix']}certificate_requirements SET certificate_id='{$_POST['certname']}',
-			req_item='{$_POST['req2']}',value='{$_POST['value2']}',table_name='{$_POST['from2']}'");
-	}
-	if ($_POST['value3']!="") {
-	mysql_query("INSERT INTO {$_SESSION['table_prefix']}certificate_requirements SET certificate_id='{$_POST['certname']}',
-			req_item='{$_POST['req3']}',value='{$_POST['value3']}',table_name='{$_POST['from3']}'");
-	}
-	if ($_POST['value4']!="") {
-	mysql_query("INSERT INTO {$_SESSION['table_prefix']}certificate_requirements SET certificate_id='{$_POST['certname']}',
-			req_item='{$_POST['req4']}',value='{$_POST['value4']}',table_name='{$_POST['from4']}'");
-	}
-	if ($_POST['value5']!="") {
-	mysql_query("INSERT INTO {$_SESSION['table_prefix']}certificate_requirements SET certificate_id='{$_POST['certname']}',
-			req_item='{$_POST['req5']}',value='{$_POST['value5']}',table_name='{$_POST['from5']}'");
-	}
-}
-header("location:{$url_root}/cert.php");
+// if ($_GET['what']=="cert") {
+// 	mysql_query("INSERT INTO {$_SESSION['table_prefix']}certificate_requirements SET certificate_id='{$_POST['certname']}',
+// 			req_item='{$_POST['req1']}',value='{$_POST['value1']}',table_name='{$_POST['from1']}'");
+// 	if ($_POST['value2']!="") {
+// 	mysql_query("INSERT INTO {$_SESSION['table_prefix']}certificate_requirements SET certificate_id='{$_POST['certname']}',
+// 			req_item='{$_POST['req2']}',value='{$_POST['value2']}',table_name='{$_POST['from2']}'");
+// 	}
+// 	if ($_POST['value3']!="") {
+// 	mysql_query("INSERT INTO {$_SESSION['table_prefix']}certificate_requirements SET certificate_id='{$_POST['certname']}',
+// 			req_item='{$_POST['req3']}',value='{$_POST['value3']}',table_name='{$_POST['from3']}'");
+// 	}
+// 	if ($_POST['value4']!="") {
+// 	mysql_query("INSERT INTO {$_SESSION['table_prefix']}certificate_requirements SET certificate_id='{$_POST['certname']}',
+// 			req_item='{$_POST['req4']}',value='{$_POST['value4']}',table_name='{$_POST['from4']}'");
+// 	}
+// 	if ($_POST['value5']!="") {
+// 	mysql_query("INSERT INTO {$_SESSION['table_prefix']}certificate_requirements SET certificate_id='{$_POST['certname']}',
+// 			req_item='{$_POST['req5']}',value='{$_POST['value5']}',table_name='{$_POST['from5']}'");
+// 	}
+// }
+// header("location:{$url_root}/cert.php");
