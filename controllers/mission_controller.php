bf0b8c83b469c7bcd394d35ea51b58c7d809d2b7
Pontus Viking <pontus.viking@meradan.se>
2014-12-05 12:09:30 +0100

    Updated create mission to use MissionController Updated MissionController with database error handling

--- controllers/mission_controller.php
+++ controllers/mission_controller.php
@@ -8,21 +8,54 @@ class MissionController {
 
   /**
    *
+   * @param Mission $mission
+   */
+  public function save($mission) {
+    $insertId = NULL;
+    $sql = "INSERT INTO uscm_mission_names SET mission_name_short=:shortName,
+                mission_name=:name, date=:date, gm=:gm, briefing=:briefing,
+                debriefing=:debriefing, platoon_id=:platoonId";
+    $stmt = $this->db->prepare($sql);
+    $stmt->bindValue(':shortName', $mission->getShortName(), PDO::PARAM_STR);
+    $stmt->bindValue(':name', $mission->getName(), PDO::PARAM_STR);
+    $stmt->bindValue(':date', $mission->getDate(), PDO::PARAM_STR);
+    $stmt->bindValue(':gm', $mission->getGmId(), PDO::PARAM_INT);
+    $stmt->bindValue(':briefing', $mission->getBriefing(),PDO::PARAM_STR);
+    $stmt->bindValue(':debriefing', $mission->getDebriefing(), PDO::PARAM_STR);
+    $stmt->bindValue(':platoonId', $mission->getPlatoonId(), PDO::PARAM_INT);
+    try {
+      $this->db->beginTransaction();
+      $stmt->execute();
+      $insertId = $this->db->lastInsertId();
+      $this->db->commit();
+    } catch (PDOException $e) {
+      $this->db->rollBack();
+    }
+    return $insertId;
+  }
+
+  /**
+   *
    * @return Mission[]
    */
   function getMissions() {
+    $missions = array();
     $sql = "SELECT mission_name_short,mission_name,mn.id as missionid,pn.name_short as platoonnameshort ".
         "FROM uscm_mission_names mn " .
         "LEFT JOIN uscm_platoon_names pn ON pn.id=mn.platoon_id ORDER BY date DESC,mission_name_short DESC";
     $stmt = $this->db->prepare($sql);
-    $stmt->execute();
-    while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
-      $mission = new Mission();
-      $mission->setId($row['missionid']);
-      $mission->setName($row['mission_name']);
-      $mission->setShortName($row['mission_name_short']);
-      $mission->setPlatoonShortName($row['platoonnameshort']);
-      $missions[] = $mission;
+    try {
+      $stmt->execute();
+      while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+        $mission = new Mission();
+        $mission->setId($row['missionid']);
+        $mission->setName($row['mission_name']);
+        $mission->setShortName($row['mission_name_short']);
+        $mission->setPlatoonShortName($row['platoonnameshort']);
+        $missions[] = $mission;
+      }
+    } catch (PDOException $e) {
+      print "Error fetching missions " . $e->getMessage() . "<br>";
     }
     return $missions;
   }
@@ -40,20 +73,22 @@ class MissionController {
         "WHERE mn.id = :missionId ORDER BY date DESC,mission_name_short DESC";
     $stmt = $this->db->prepare($sql);
     $stmt->bindValue(':missionId', $missionId, PDO::PARAM_INT);
-    $stmt->execute();
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
     $mission = new Mission();
-    if ($row['howmany'] == 1) {
-      $mission->setId($row['missionid']);
-      $mission->setName($row['mission_name']);
-      $mission->setShortName($row['mission_name_short']);
-      $mission->setPlatoonShortName($row['platoonnameshort']);
-      $mission->setGmId($row['gm']);
-      $mission->setDate($row['date']);
-      $mission->setBriefing($row['briefing']);
-      $mission->setDebriefing($row['debriefing']);
-      $mission->setPlatoonId($row['platoon_id']);
-
+    try {
+      $stmt->execute();
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      if ($row['howmany'] == 1) {
+        $mission->setId($row['missionid']);
+        $mission->setName($row['mission_name']);
+        $mission->setShortName($row['mission_name_short']);
+        $mission->setPlatoonShortName($row['platoonnameshort']);
+        $mission->setGmId($row['gm']);
+        $mission->setDate($row['date']);
+        $mission->setBriefing($row['briefing']);
+        $mission->setDebriefing($row['debriefing']);
+        $mission->setPlatoonId($row['platoon_id']);
+      }
+    } catch (PDOException $e) {
     }
     return $mission;
   }
@@ -72,8 +107,12 @@ class MissionController {
                   WHERE m.mission_id=:missionId ORDER BY c.lastname,c.forname";
     $stmt = $this->db->prepare($sql);
     $stmt->bindValue(':missionId', $mission->getId(), PDO::PARAM_INT);
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    try {
+      $stmt->execute();
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+      return array();
+    }
   }
 
   /**
@@ -89,8 +128,12 @@ class MissionController {
                   WHERE m.mission_id=:missionId";
     $stmt = $this->db->prepare($sql);
     $stmt->bindValue(':missionId', $mission->getId(), PDO::PARAM_INT);
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    try {
+      $stmt->execute();
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+      return array();
+    }
   }
 
   /**
@@ -106,8 +149,12 @@ class MissionController {
                   WHERE m.mission_id=:missionId";
     $stmt = $this->db->prepare($sql);
     $stmt->bindValue(':missionId', $mission->getId(), PDO::PARAM_INT);
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    try {
+      $stmt->execute();
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+      return array();
+    }
   }
 
   /**
@@ -132,13 +179,17 @@ class MissionController {
     $sql = $sql . ") ORDER BY mn.medal_glory DESC";
     $stmt = $this->db->prepare($sql);
     $stmt->bindValue(':missionId', $mission->getId(), PDO::PARAM_INT);
-    $stmt->execute();
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($row) {
-      return $row['medalid'];
-    } else {
-      return 0;
+    try {
+      $stmt->execute();
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      if ($row) {
+        return $row['medalid'];
+      }
+    } catch (PDOException $e) {
+      return array();
     }
+
+    return 0;
   }
 
   /**
@@ -152,8 +203,11 @@ class MissionController {
     $stmt = $this->db->prepare($sql);
     $stmt->bindValue(':missionId', $mission->getId(), PDO::PARAM_INT);
     $stmt->bindValue(':characterId', $character->getId(), PDO::PARAM_INT);
-    $stmt->execute();
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    return $row['rank_id'];
+    try {
+      $stmt->execute();
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      return $row['rank_id'];
+    } catch (PDOException $e) {
+    }
   }
 }
--- mission.php
+++ mission.php
@@ -1,157 +1,164 @@
-<?php 
+<?php
 /*
  *	Functions handling missions should go in here
- *	Care should be taken when removing a character from a mission as promotions and 
+ *	Care should be taken when removing a character from a mission as promotions and
  *	commendations could be screwed up
  *
  */
 session_start();
 include("functions.php");
+$missionController = new MissionController();
+
 myconnect();
 mysql_select_db("skynet");
 
 if ($_GET['what']=="names") {
-	$short=strtr(htmlspecialchars($_POST['mission_name_short'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n")); 
-	$name=strtr(htmlspecialchars($_POST['mission_name'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
-	$date=strtr(htmlspecialchars($_POST['date'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
-	$platoon_id=$_POST['platoon_id'];
-	$sql="UPDATE {$_SESSION['table_prefix']}mission_names 
-		SET mission_name_short='{$short}',mission_name='{$name}',date='{$date}',platoon_id='{$platoon_id}'
-		WHERE id='{$_GET['mission']}'";
-	mysql_query($sql);
+  $short=strtr(htmlspecialchars($_POST['mission_name_short'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
+  $name=strtr(htmlspecialchars($_POST['mission_name'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
+  $date=strtr(htmlspecialchars($_POST['date'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
+  $platoon_id=$_POST['platoon_id'];
+  $sql="UPDATE {$_SESSION['table_prefix']}mission_names
+    SET mission_name_short='{$short}',mission_name='{$name}',date='{$date}',platoon_id='{$platoon_id}'
+    WHERE id='{$_GET['mission']}'";
+  mysql_query($sql);
 }
 elseif ($_GET['what']=="gm") {
-	$sql="UPDATE {$_SESSION['table_prefix']}mission_names 
-		SET gm='{$_POST['gm']}' WHERE id='{$_GET['mission']}'";
-	mysql_query($sql);
+  $sql="UPDATE {$_SESSION['table_prefix']}mission_names
+    SET gm='{$_POST['gm']}' WHERE id='{$_GET['mission']}'";
+  mysql_query($sql);
 }
-elseif ($_GET['what']=="briefing") { 
-	$briefing=strtr(htmlspecialchars($_POST['briefing'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
-	$sql="UPDATE {$_SESSION['table_prefix']}mission_names 
-		SET briefing='{$briefing}'
-		WHERE id='{$_GET['mission']}'";
-	mysql_query($sql);
+elseif ($_GET['what']=="briefing") {
+  $briefing=strtr(htmlspecialchars($_POST['briefing'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
+  $sql="UPDATE {$_SESSION['table_prefix']}mission_names
+    SET briefing='{$briefing}'
+    WHERE id='{$_GET['mission']}'";
+  mysql_query($sql);
 }
-elseif ($_GET['what']=="debriefing") { 
-	$debriefing=strtr(htmlspecialchars($_POST['debriefing'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
-	$sql="UPDATE {$_SESSION['table_prefix']}mission_names 
-		SET debriefing='{$debriefing}'
-		WHERE id='{$_GET['mission']}'";
-	mysql_query($sql);
+elseif ($_GET['what']=="debriefing") {
+  $debriefing=strtr(htmlspecialchars($_POST['debriefing'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
+  $sql="UPDATE {$_SESSION['table_prefix']}mission_names
+    SET debriefing='{$debriefing}'
+    WHERE id='{$_GET['mission']}'";
+  mysql_query($sql);
 }
-elseif ($_GET['what']=="characters") { 
-	$remove_characters=array();
-	$add_characters=array();
-	$old_characters=array();
-	$mission_id=$_GET['mission'];
-	$table=$_SESSION['table_prefix'];
-	
-	//
-	// Characters on a mission
-	//
-	// Finds all character currently in database for the mission
-	$sql="select character_id,id,mission_id from {$table}missions where mission_id={$mission_id}";
-	$characterres=mysql_query($sql) or die(mysql_error());
-	while($character=mysql_fetch_array($characterres)){
-		$old_characters[$character['character_id']][mission_id]=$character['mission_id'];
-		$old_characters[$character['character_id']][id]=$character['id'];
-	}
-	// walks through $_POST[] and decides what to delete and insert in database
-	foreach($_POST['characters'] as $character_id) {
-		if($old_characters[$character_id]) {
-			// remove the handled data from old_characters since it's already in the database
-			unset($old_characters[$character_id]);
-		} else {
-			// new data, add it
-			$add_characters[$character_id]=$mission_id;
-		}
-	}
-	// remove the characters that weren't in the $_POST, and thereby were removed from the mission
-	foreach($old_characters as $index => $id){
-		
-		$remove_characters[$id[id]]=$id[id];
-		unset($old_characters[$index]);
-	}
-	foreach($remove_characters as  $id) {
-		$sql="DELETE FROM {$table}missions WHERE id='{$id}' LIMIT 1";
-		mysql_query($sql) or die("tabort: " . mysql_error());
-	}
-	foreach($add_characters as $character_id => $mission_id) {
-		$sql="INSERT INTO {$table}missions SET character_id='{$character_id}',mission_id='{$mission_id}'";
-		mysql_query($sql);
-	}
+elseif ($_GET['what']=="characters") {
+  $remove_characters=array();
+  $add_characters=array();
+  $old_characters=array();
+  $mission_id=$_GET['mission'];
+  $table=$_SESSION['table_prefix'];
+
+  //
+  // Characters on a mission
+  //
+  // Finds all character currently in database for the mission
+  $sql="select character_id,id,mission_id from {$table}missions where mission_id={$mission_id}";
+  $characterres=mysql_query($sql) or die(mysql_error());
+  while($character=mysql_fetch_array($characterres)){
+    $old_characters[$character['character_id']][mission_id]=$character['mission_id'];
+    $old_characters[$character['character_id']][id]=$character['id'];
+  }
+  // walks through $_POST[] and decides what to delete and insert in database
+  foreach($_POST['characters'] as $character_id) {
+    if($old_characters[$character_id]) {
+      // remove the handled data from old_characters since it's already in the database
+      unset($old_characters[$character_id]);
+    } else {
+      // new data, add it
+      $add_characters[$character_id]=$mission_id;
+    }
+  }
+  // remove the characters that weren't in the $_POST, and thereby were removed from the mission
+  foreach($old_characters as $index => $id){
+
+    $remove_characters[$id[id]]=$id[id];
+    unset($old_characters[$index]);
+  }
+  foreach($remove_characters as  $id) {
+    $sql="DELETE FROM {$table}missions WHERE id='{$id}' LIMIT 1";
+    mysql_query($sql) or die("tabort: " . mysql_error());
+  }
+  foreach($add_characters as $character_id => $mission_id) {
+    $sql="INSERT INTO {$table}missions SET character_id='{$character_id}',mission_id='{$mission_id}'";
+    mysql_query($sql);
+  }
 
 }
-elseif ($_GET['what']=="commendations") { 
-	foreach ($_POST['characters'] as $character_id => $dummy) {
-		//check for foreign respective national medals 
-		$sql="SELECT mn.id,mn.foreign_medal FROM {$_SESSION['table_prefix']}medal_names mn 
-					LEFT JOIN {$_SESSION['table_prefix']}missons m ON mn.id=m.medal_id
-					WHERE m.character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
-		$sqlres=mysql_query($sql);
-		
-		$sql="UPDATE {$_SESSION['table_prefix']}missions SET medal_id='{$_POST['medal']}' WHERE character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
-		mysql_query($sql);
-	}
+elseif ($_GET['what']=="commendations") {
+  foreach ($_POST['characters'] as $character_id => $dummy) {
+    //check for foreign respective national medals
+    $sql="SELECT mn.id,mn.foreign_medal FROM {$_SESSION['table_prefix']}medal_names mn
+          LEFT JOIN {$_SESSION['table_prefix']}missons m ON mn.id=m.medal_id
+          WHERE m.character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
+    $sqlres=mysql_query($sql);
+
+    $sql="UPDATE {$_SESSION['table_prefix']}missions SET medal_id='{$_POST['medal']}' WHERE character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
+    mysql_query($sql);
+  }
 }
-elseif ($_GET['what']=="create_mission") { 
-	$short=strtr(htmlspecialchars($_POST['mission'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n")); 
-	$name=strtr(htmlspecialchars($_POST['name'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
-	$date=strtr(htmlspecialchars($_POST['date'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
-	$briefing=strtr(htmlspecialchars($_POST['briefing'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
-	$debriefing=strtr(htmlspecialchars($_POST['debriefing'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
-	$platoon_id=$_POST['platoon_id'];
-	$sql="INSERT INTO {$_SESSION['table_prefix']}mission_names SET mission_name_short='{$short}',mission_name='{$name}',date='{$date}',gm='{$_SESSION['user_id']}',briefing='{$briefing}', debriefing='{$debriefing}',platoon_id='{$platoon_id}'";
-	mysql_query($sql);
-	$mission=mysql_insert_id();
-	$_GET['mission']=$mission;	
+elseif ($_GET['what']=="create_mission") {
+  $short=strtr(htmlspecialchars($_POST['mission'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
+  $name=strtr(htmlspecialchars($_POST['name'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
+  $date=strtr(htmlspecialchars($_POST['date'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
+  $briefing=strtr(htmlspecialchars($_POST['briefing'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
+  $debriefing=strtr(htmlspecialchars($_POST['debriefing'], ENT_QUOTES, "UTF-8"),array("\n"=>"<br/>\n"));
+  $platoon_id=$_POST['platoon_id'];
+  $mission = new Mission();
+  $mission->setName($name);
+  $mission->setShortName($short);
+  $mission->setDate($date);
+  $mission->setBriefing($briefing);
+  $mission->setDebriefing($debriefing);
+  $mission->setPlatoonId($platoon_id);
+  $missionId = $missionController->save($mission);
+  $_GET['mission'] = $missionId;
 }
 elseif ($_GET['what']=="promotion") {
-	$character_id=$_POST['character'];
-	// The rank for promotion has been selected, time to insert into 
-	// the database
-	if ($_POST['rank']) {
-		$sql="UPDATE {$_SESSION['table_prefix']}missions SET rank_id='{$_POST['rank']}' WHERE character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
-		mysql_query($sql);
-		$sql="UPDATE {$_SESSION['table_prefix']}ranks SET rank_id='{$_POST['rank']}' WHERE character_id='{$character_id}'";
-		mysql_query($sql);
-	} 
-	// A character that previously was selected for promotion on the mission
-	// which has been withdrawn. Previous rank should be restored.
-	else {
-		// Select the rank that was choosen for this mission
-		$sql="SELECT rank_id FROM {$_SESSION['table_prefix']}missions WHERE character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
-		$sqlres=mysql_query($sql);
-		$formerrank=mysql_fetch_array($sqlres);
-		// Select the previous rank that the character held before this mission, or rather
-		// the promotion the character had gotten before. If no promotion, result is empty
-		$missiondate=mysql_fetch_array(mysql_query("SELECT date FROM {$_SESSION['table_prefix']}mission_names WHERE id='{$_GET['mission']}'"));
-		$sql="SELECT m.rank_id FROM {$_SESSION['table_prefix']}missions m LEFT JOIN {$_SESSION['table_prefix']}mission_names mn on mn.id =m.mission_id WHERE character_id='{$character_id}' AND date<'{$missiondate['date']}' ORDER BY date DESC LIMIT 1";
-		$sqlres=mysql_query($sql);
-		$rankbeforeformerrank=mysql_fetch_array($sqlres);
-		// The character has been promoted before and should retain that rank
-		if ($rankbeforeformerrank['rank_id']) {
-			$sql="UPDATE {$_SESSION['table_prefix']}ranks SET rank_id='{$rankbeforeformerrank['rank_id']}' WHERE character_id='{$character_id}'";
-			mysql_query($sql);
-			$sql="UPDATE {$_SESSION['table_prefix']}missions SET rank_id='{$_POST['rank']}' WHERE character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
-			mysql_query($sql);
-		} 
-		// The character have not been promoted before the mission and should be restored to the rank
-		// the character had upon joining the corps, i.e. LCpl or Pvt. This however isn't consistent with 
-		// characters who has the military family advantage.
-		else {
-			if ($formerrank['rank_id']>2) {
-				$rank = 3; //LCpl
-			}
-			else {
-				$rank = 1; //Pvt
-			}
-			$sql="UPDATE {$_SESSION['table_prefix']}ranks SET rank_id='{$rank}' WHERE character_id='{$character_id}'";
-			mysql_query($sql);
-			$sql="UPDATE {$_SESSION['table_prefix']}missions SET rank_id='{$_POST['rank']}' WHERE character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
-			mysql_query($sql);
-		}
-	}
+  $character_id=$_POST['character'];
+  // The rank for promotion has been selected, time to insert into
+  // the database
+  if ($_POST['rank']) {
+    $sql="UPDATE {$_SESSION['table_prefix']}missions SET rank_id='{$_POST['rank']}' WHERE character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
+    mysql_query($sql);
+    $sql="UPDATE {$_SESSION['table_prefix']}ranks SET rank_id='{$_POST['rank']}' WHERE character_id='{$character_id}'";
+    mysql_query($sql);
+  }
+  // A character that previously was selected for promotion on the mission
+  // which has been withdrawn. Previous rank should be restored.
+  else {
+    // Select the rank that was choosen for this mission
+    $sql="SELECT rank_id FROM {$_SESSION['table_prefix']}missions WHERE character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
+    $sqlres=mysql_query($sql);
+    $formerrank=mysql_fetch_array($sqlres);
+    // Select the previous rank that the character held before this mission, or rather
+    // the promotion the character had gotten before. If no promotion, result is empty
+    $missiondate=mysql_fetch_array(mysql_query("SELECT date FROM {$_SESSION['table_prefix']}mission_names WHERE id='{$_GET['mission']}'"));
+    $sql="SELECT m.rank_id FROM {$_SESSION['table_prefix']}missions m LEFT JOIN {$_SESSION['table_prefix']}mission_names mn on mn.id =m.mission_id WHERE character_id='{$character_id}' AND date<'{$missiondate['date']}' ORDER BY date DESC LIMIT 1";
+    $sqlres=mysql_query($sql);
+    $rankbeforeformerrank=mysql_fetch_array($sqlres);
+    // The character has been promoted before and should retain that rank
+    if ($rankbeforeformerrank['rank_id']) {
+      $sql="UPDATE {$_SESSION['table_prefix']}ranks SET rank_id='{$rankbeforeformerrank['rank_id']}' WHERE character_id='{$character_id}'";
+      mysql_query($sql);
+      $sql="UPDATE {$_SESSION['table_prefix']}missions SET rank_id='{$_POST['rank']}' WHERE character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
+      mysql_query($sql);
+    }
+    // The character have not been promoted before the mission and should be restored to the rank
+    // the character had upon joining the corps, i.e. LCpl or Pvt. This however isn't consistent with
+    // characters who has the military family advantage.
+    else {
+      if ($formerrank['rank_id']>2) {
+        $rank = 3; //LCpl
+      }
+      else {
+        $rank = 1; //Pvt
+      }
+      $sql="UPDATE {$_SESSION['table_prefix']}ranks SET rank_id='{$rank}' WHERE character_id='{$character_id}'";
+      mysql_query($sql);
+      $sql="UPDATE {$_SESSION['table_prefix']}missions SET rank_id='{$_POST['rank']}' WHERE character_id='{$character_id}' AND mission_id='{$_GET['mission']}'";
+      mysql_query($sql);
+    }
+  }
 }
 
 header("location:{$url_root}/index.php?url=show_mission.php&id={$_GET['mission']}");
