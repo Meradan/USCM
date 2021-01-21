013a6287274a4ae87f04e45d4b0d1e23a9d2d9b5
Oskar Grindemyr <oskar.grindemyr@gmail.com>
2021-01-21 15:55:14 +0100

    Fixed HoF glory display bug

--- list_hall_of_fame.php
+++ list_hall_of_fame.php
@@ -10,51 +10,51 @@ if (array_key_exists('platoon', $_GET)) {
 }
 $deadcharactersql="SELECT c.id as cid,c.forname as cfor,c.lastname as clast,DATE_FORMAT(c.enlisted,'%y-%m-%d') as enlisted,c.status,
               rank_short,rank_id,specialty_name, p.forname,p.lastname,c.userid,c.status_desc
-          FROM {$_SESSION['table_prefix']}characters c
+          FROM uscm_characters c
           LEFT JOIN Users as p ON c.userid = p.id
-          LEFT JOIN {$_SESSION['table_prefix']}ranks
-            ON {$_SESSION['table_prefix']}ranks.character_id = c.id
-          LEFT JOIN {$_SESSION['table_prefix']}rank_names
-            ON {$_SESSION['table_prefix']}rank_names.id =
-              {$_SESSION['table_prefix']}ranks.rank_id
-          LEFT JOIN {$_SESSION['table_prefix']}specialty
-            ON {$_SESSION['table_prefix']}specialty.character_id = c.id
-          LEFT JOIN {$_SESSION['table_prefix']}specialty_names
-            ON {$_SESSION['table_prefix']}specialty_names.id =
-              {$_SESSION['table_prefix']}specialty.specialty_name_id
+          LEFT JOIN uscm_ranks
+            ON uscm_ranks.character_id = c.id
+          LEFT JOIN uscm_rank_names
+            ON uscm_rank_names.id =
+              uscm_ranks.rank_id
+          LEFT JOIN uscm_specialty
+            ON uscm_specialty.character_id = c.id
+          LEFT JOIN uscm_specialty_names
+            ON uscm_specialty_names.id =
+              uscm_specialty.specialty_name_id
               WHERE c.status = 'Dead' AND c.userid != '0' {$where}
               ORDER BY rank_id DESC";
 $retiredcharactersql="SELECT c.id as cid,c.forname as cfor,c.lastname as clast,DATE_FORMAT(c.enlisted,'%y-%m-%d') as enlisted,c.status,
               rank_short,rank_id,specialty_name, p.forname,p.lastname,c.userid,c.status_desc
-          FROM {$_SESSION['table_prefix']}characters c
+          FROM uscm_characters c
           LEFT JOIN Users as p ON c.userid = p.id
-          LEFT JOIN {$_SESSION['table_prefix']}ranks
-            ON {$_SESSION['table_prefix']}ranks.character_id = c.id
-          LEFT JOIN {$_SESSION['table_prefix']}rank_names
-            ON {$_SESSION['table_prefix']}rank_names.id =
-              {$_SESSION['table_prefix']}ranks.rank_id
-          LEFT JOIN {$_SESSION['table_prefix']}specialty
-            ON {$_SESSION['table_prefix']}specialty.character_id = c.id
-          LEFT JOIN {$_SESSION['table_prefix']}specialty_names
-            ON {$_SESSION['table_prefix']}specialty_names.id =
-              {$_SESSION['table_prefix']}specialty.specialty_name_id
+          LEFT JOIN uscm_ranks
+            ON uscm_ranks.character_id = c.id
+          LEFT JOIN uscm_rank_names
+            ON uscm_rank_names.id =
+              uscm_ranks.rank_id
+          LEFT JOIN uscm_specialty
+            ON uscm_specialty.character_id = c.id
+          LEFT JOIN uscm_specialty_names
+            ON uscm_specialty_names.id =
+              uscm_specialty.specialty_name_id
               WHERE c.status = 'Retired' {$where}
               ORDER BY rank_id DESC";
-$platoonsql="SELECT id,name_short,name_long FROM {$_SESSION['table_prefix']}platoon_names";
+$platoonsql="SELECT id,name_short,name_long FROM uscm_platoon_names";
 $glorytopsql="SELECT c.id as cid,c.forname as cfor,c.lastname as clast,DATE_FORMAT(c.enlisted,'%y-%m-%d') as enlisted,c.status,
               rank_short,rank_id,specialty_name, p.forname,p.lastname,c.userid,c.status
-          FROM {$_SESSION['table_prefix']}characters c
+          FROM uscm_characters c
           LEFT JOIN Users as p ON c.userid = p.id
-          LEFT JOIN {$_SESSION['table_prefix']}ranks
-            ON {$_SESSION['table_prefix']}ranks.character_id = c.id
-          LEFT JOIN {$_SESSION['table_prefix']}rank_names
-            ON {$_SESSION['table_prefix']}rank_names.id =
-              {$_SESSION['table_prefix']}ranks.rank_id
-          LEFT JOIN {$_SESSION['table_prefix']}specialty
-            ON {$_SESSION['table_prefix']}specialty.character_id = c.id
-          LEFT JOIN {$_SESSION['table_prefix']}specialty_names
-            ON {$_SESSION['table_prefix']}specialty_names.id =
-              {$_SESSION['table_prefix']}specialty.specialty_name_id
+          LEFT JOIN uscm_ranks
+            ON uscm_ranks.character_id = c.id
+          LEFT JOIN uscm_rank_names
+            ON uscm_rank_names.id =
+              uscm_ranks.rank_id
+          LEFT JOIN uscm_specialty
+            ON uscm_specialty.character_id = c.id
+          LEFT JOIN uscm_specialty_names
+            ON uscm_specialty_names.id =
+              uscm_specialty.specialty_name_id
               WHERE c.userid != '0' {$where}
               ORDER BY rank_id DESC";
 //echo $retiredcharactersql . "<br><br><br><br><br><br>";
@@ -89,10 +89,19 @@ foreach ($platoons as $platoon) { ?>
     $i = 0;
     foreach ($characterarray as $character) {
     $i++;
-    if ($i >= 7) { break; }?>
+    if ($i >= 7) { break; }
+	
+	$medals = "";
+	$glory = 0;
+	$commendationsArray = getCommendationsForCharacter($character['cid']);
+	foreach ($commendationsArray as $key => $value) {
+		if ($commendationsArray[$key]['medal_short'] != "") $medals = $medals . " " . $commendationsArray[$key]['medal_short'];
+		$glory = $glory + $commendationsArray[$key]['medal_glory'];
+	}
+	?>
   <TR>
-    <TD><?php echo $character['glory'];?></TD>
-    <TD><?php echo $character['medals'];?></TD>
+    <TD><?php echo $glory;?></TD>
+    <TD><?php echo $medals;?></TD>
     <TD><?php echo $character['missions'];?></TD>
     <TD><?php echo $character['rank_short'];?></TD>
     <TD><?php if ($admin || $gm || $_SESSION['user_id']==$character['userid']) {
@@ -140,7 +149,7 @@ foreach ($platoons as $platoon) { ?>
     <TD><?php echo $character['forname'] . " " . $character['lastname'];?></TD>
     <TD><?php echo $character['enlisted'];?></TD>
 <?php
-// $lastmissionsql="SELECT DATE_FORMAT(date,'%y-%m-%d') as date,mission_name_short FROM {$_SESSION['table_prefix']}mission_names LEFT JOIN {$_SESSION['table_prefix']}missions as m on m.mission_id = {$_SESSION['table_prefix']}mission_names.id WHERE character_id = '{$character['cid']}' ORDER BY date DESC LIMIT 1";
+// $lastmissionsql="SELECT DATE_FORMAT(date,'%y-%m-%d') as date,mission_name_short FROM uscm_mission_names LEFT JOIN uscm_missions as m on m.mission_id = uscm_mission_names.id WHERE character_id = '{$character['cid']}' ORDER BY date DESC LIMIT 1";
 //     $lastmission=mysql_fetch_array(mysql_query($lastmissionsql));
     $lastMission = lastMissionForCharacter($character['cid'])?>
     <TD><?php echo $lastMission['date'];?></TD>
@@ -189,7 +198,7 @@ foreach ($platoons as $platoon) { ?>
     <TD><?php echo $character['forname'] . " " . $character['lastname'];?></TD>
     <TD><?php echo $character['enlisted'];?></TD>
 <?php
-// $lastmissionsql="SELECT DATE_FORMAT(date,'%y-%m-%d') as date,mission_name_short FROM {$_SESSION['table_prefix']}mission_names LEFT JOIN {$_SESSION['table_prefix']}missions as m on m.mission_id = {$_SESSION['table_prefix']}mission_names.id WHERE character_id = '{$character['cid']}' ORDER BY date DESC LIMIT 1";
+// $lastmissionsql="SELECT DATE_FORMAT(date,'%y-%m-%d') as date,mission_name_short FROM uscm_mission_names LEFT JOIN uscm_missions as m on m.mission_id = uscm_mission_names.id WHERE character_id = '{$character['cid']}' ORDER BY date DESC LIMIT 1";
 // 		$lastMission=mysql_fetch_array(mysql_query($lastmissionsql));
     $lastMission = lastMissionForCharacter($character['cid']) ?>
     <TD><?php echo $lastMission['date'];?></TD>
