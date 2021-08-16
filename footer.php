3cab68019f94590f1ff16a4cdb4444807da280a8
uscmbrixton <oskar.grindemyr@gmail.com>
2021-08-16 12:47:12 +0200

    Email update

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div>
 <BR><CENTER><IMG SRC="images/line.jpg" WIDTH="449" HEIGHT="1"></CENTER><BR>
-<SMALL><CENTER>Campaign & Content: <A HREF="mailto:oskar.grindemyr+uscm@gmail.com?subject=USCM">Oskar</A> &#160;&#160;&#160;|&#160;&#160;&#160; Web Production: <A HREF="mailto:tottemans@hotmail.com?subject=USCM">Totte</A> &#160;&#160;&#160;|&#160;&#160;&#160; Database Production: <A HREF="mailto:meradan+uscm@gmail.com?subject=USCM">Pontus</A></CENTER></SMALL>
+<SMALL><CENTER>Campaign & Content: <A HREF="mailto:oskar.grindemyr@gmail.com?subject=USCM">Oskar</A> &#160;&#160;&#160;|&#160;&#160;&#160; Web Production: <A HREF="mailto:tottemans@hotmail.com?subject=USCM">Totte</A> &#160;&#160;&#160;|&#160;&#160;&#160; Database Production: <A HREF="mailto:meradan+uscm@gmail.com?subject=USCM">Pontus</A></CENTER></SMALL>
 
 </DIV><BR>
 <div style="font-size:smaller; text-align:center;">All content are copyright to their respective owners</div
