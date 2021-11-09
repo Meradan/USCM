e0d86a180fa98095e207f40f29c57265592f837e
uscmbrixton <oskar.grindemyr@gmail.com>
2021-11-09 13:32:22 +0100

    Added a discord link

--- menu.php
+++ menu.php
@@ -14,9 +14,8 @@
 <a href="index.php?url=list_characters.php">Characters</a> | 
 <a href="index.php?url=list_missions.php">Missions</a> | 
 <a href="index.php?url=list_hall_of_fame.php">Hall of Fame</a> |
-<?php //<a href="../forum/index.php">Forum</a>
-    ?>
 <a href="https://uscm.swedishforum.net/" target="_blank">Forum</a>
+<a href="https://discord.gg/Ym9kR7g5y4" target="_blank">Discord</a>
 <?php echo ($_SESSION['level']>=2)?(' | <a href="index.php?url=create_character.php">Create character</a>'):(""); ?>
 <?php echo ($_SESSION['level']>=2)?(' | <a href="index.php?url=create_mission.php">Create mission</a>'):(""); ?>
 <?php if ($_SESSION['level']==3) {
