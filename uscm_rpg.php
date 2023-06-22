808ba29ebc238258f165c19730b4d438f0110095
uscmbrixton <oskar.grindemyr@gmail.com>
2023-06-22 17:51:38 +0200

    Text update

--- uscm_rpg.php
+++ uscm_rpg.php
@@ -4,7 +4,7 @@
 
 Det här är en rollspelskampanj i militär science fiction-miljö inspirerad främst av Alien-filmerna, men även Starship Troopers, XCOM m.m. Spelarnas karaktärer är soldater i framtidens motsvarighet till den amerikanska marinkåren. Stämningen i kampanjen inriktas i första hand på action, spänning och taktisk planering, men det kan vara stor variation mellan olika uppdrag.<BR><BR>
 
-Kampanjen har ingen fast spelargrupp. Den är istället organiserad på ett sätt som låter många fler spelare vara med i den.
+Kampanjen har ingen fast spelargrupp. Den är istället organiserad på ett sätt som låter fler spelare vara med i den.
 Varje uppdrag är ett fristående scenario. Alla spelare är med på en maillista och när en spelledare har gjort ett nytt äventyr
 skickas ett "announcement" för detta ut på maillistan. Detta innehåller tid och plats, antal spelare samt en kort beskrivning
 av uppdraget. Sedan är det först till kvarn. De första spelarna som anmäler sig får en mer detaljerad briefing som bekräftelse
@@ -12,10 +12,11 @@ på att de har kommit med på uppdraget.<BR> Det gör att det blir variation i s
 vara med i kampanjen även om man inte kan spela så ofta. Ibland när alldeles för många
 har velat vara med på samma uppdrag har det spelats flera gånger med olika spelargrupper.<BR><BR>
 
-Första uppdraget i kampanjen spelades i oktober år 2000. Därefter har det blivit många fler och efter en period av cryosleep är vi nu igång igen om än i lägre tempo än tidigare. Alla uppdrag finns beskrivna under "Missions" med den
-briefing-text som spelarna fick ut innan uppdraget och med sammanfattningen som skrivits efteråt av spelledaren eller någon spelare.<BR><BR>
+Alla uppdrag som spelats inom kampanjen finns beskrivna under "Missions" med den
+briefing-text som spelarna fick ut innan uppdraget och med sammanfattningen som skrivits efteråt av spelledaren eller någon spelare. Scenarion vi kör på konvent brukar inte få egna debriefingar, men dom spelas ofta som ett kampanjmission också.<BR><BR>
 
-Kampanjen håller mestadels till i Linköping där den startade. Men det finns även mer eller mindre aktiva spelgrupper (plutoner) på andra orter. USCM brukar även dyka upp som arrangemang på spelkonventet <A HREF="http://www.lincon.se" TARGET="_blank">LinCon</A>.<BR><BR>
+Kampanjen håller mestadels till i Linköping där den startade. Det har periodvis även funnits aktiva spelgrupper (plutoner) på andra orter och på senare tid kör vi ibland missions över mail också. USCM brukar även dyka upp som arrangemang på spelkonventet <A HREF="http://www.lincon.se" TARGET="_blank">LinCon</A>.<BR><BR>
+Det finns för tillfället plats för fler spelare, kom in på vår Discord om du är intresserad.<BR><BR>
 
 <br/><img src="images/linje.jpg" width="449" height="1" align="middle"><br/>
 
@@ -25,11 +26,14 @@ This is the homepage for a roleplaying game set in a universe inspired by the Al
 The goal of this campaign is to mix the atmosphere of the Alien movies with a military-style RPG campaign where
 we focus on action and tactical planning.<BR><BR>
 
-All roleplaying is done in the traditional pen & paper style - there is no online roleplaying here. The campaign is based in Sweden, but recently we were also joined by a english-speaking group of players in Canada.<BR><BR>
+Most roleplaying is done in the traditional pen & paper style, but there are occasional PBEM scenarios as well.<BR><BR>
 
 <br/><img src="images/line.jpg" width="449" height="1" align="middle"><br/>
 
-<div class="title">Downloads</div>
+<div class="title">USCM v3 Resources</div>
+Coming soon...<BR><BR>
+
+<div class="title">USCM v2 Resources (old edition)</div>
 To use the Character Generator you need LibreOffice (free, open source) which can be downloaded from <a href="http://www.libreoffice.org" target="_blank">www.libreoffice.org</a>
 <br/>
 You need to activate macros under Tools->Options->LibreOffice->Security. A good way to do it is to set the folder with the Character generator in it as a "Trusted Source".
@@ -43,9 +47,11 @@ You need to activate macros under Tools->Options->LibreOffice->Security. A good 
 <br/>
 <A HREF="resources/USCM-Rules2.pdf" TARGET="_blank">Rules - Properties/Cert</A> (PDF, description of all properties etc.)<br/>
 <br/>
+<br/><img src="images/linje.jpg" width="449" height="1" align="middle"><br/>
+
+<div class="title">Other campaign material</div>
 <A HREF="resources/InofficiellBriefingBF5.pdf" TARGET="_blank">Unofficial briefing</A> (BF5 campaign knowledge about aliens, in swedish)<br/>
 <br/>
-<br/><img src="images/linje.jpg" width="449" height="1" align="middle"><br/>
 
 <div class="title">Creators</div>
 Campaign &amp; Content: Oskar (<A HREF="mailto:oskar.grindemyr@gmail.com?subject=USCM">oskar.grindemyr@gmail.com</A>)<BR>
