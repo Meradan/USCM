53e2873297aeb85b1dce21fcadefeea73901502c
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-08-02 13:18:35 +0200

    feat: remove old domain from header

--- menu.php
+++ menu.php
@@ -9,27 +9,24 @@
 <TABLE BACKGROUND="images/logo_menu_index.jpg" ALIGN="center" WIDTH="627" HEIGHT="20">
 <TR><TD WIDTH="607" ALIGN="center" class="colorfont" style="text-align:center">
 
-<a href="index.php?url=news.php">News</a> | 
-<a href="index.php?url=uscm_rpg.php">USCM RPG</a> | 
-<a href="index.php?url=list_characters.php">Characters</a> | 
-<a href="index.php?url=list_missions.php">Missions</a> | 
+<a href="index.php?url=news.php">News</a> |
+<a href="index.php?url=uscm_rpg.php">USCM RPG</a> |
+<a href="index.php?url=list_characters.php">Characters</a> |
+<a href="index.php?url=list_missions.php">Missions</a> |
 <a href="index.php?url=list_hall_of_fame.php">Hall of Fame</a> |
-<a href="https://uscm.swedishforum.net/" target="_blank">Forum</a>
+<a href="https://uscm.swedishforum.net/" target="_blank">Forum</a> |
 <a href="https://discord.gg/nEp7kwd4h7" target="_blank">Discord</a>
 <?php echo ($_SESSION['level']>=2)?(' | <a href="index.php?url=create_character.php">Create character</a>'):(""); ?>
 <?php echo ($_SESSION['level']>=2)?(' | <a href="index.php?url=create_mission.php">Create mission</a>'):(""); ?>
 <?php if ($_SESSION['level']==3) {
-	echo ' | <a href="index.php?url=modify_player.php">Modify player</a>'; 
+	echo ' | <a href="index.php?url=modify_player.php">Modify player</a>';
 } elseif ($_SESSION['level']>=1) {
-	echo " | <a href=\"index.php?url=modify_player.php&player={$_SESSION['user_id']}\">Modify player</a>"; 
+	echo " | <a href=\"index.php?url=modify_player.php&player={$_SESSION['user_id']}\">Modify player</a>";
 }	?>
 <?php echo ($_SESSION['level']>=3)?(' | <a href="index.php?url=create_player.php">Create player</a>'):(""); ?>
 <?php echo ($_SESSION['inloggad']==1)?(' | <a href="login.php?alt=logout">Log Out</a>'):('| <a href="login2.php?alt=login">Log In</a>'); ?>
 </TD>
 </TR></TABLE>
-
-<TABLE BACKGROUND="images/logo_bottom_index.jpg" ALIGN="center" WIDTH="627" HEIGHT="24">
-<TR><TD WIDTH="10"></TD><TD WIDTH="607"></TD><TD WIDTH="10"></TD></TR></TABLE>
 
 <BR><CENTER><IMG SRC="images/line.jpg" WIDTH="449" HEIGHT="1"></CENTER><BR>
 <div>
