6a03912689b47d9f45bf129e9b4a3346ea7ef622
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-08-06 15:28:41 +0200

    feat(styling): horizontal ruler style

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <footer class="footer">
-<CENTER><IMG SRC="images/line.jpg" WIDTH="449" HEIGHT="1"></CENTER><BR>
-<SMALL><CENTER>Campaign & Content: <A HREF="mailto:oskar.grindemyr@gmail.com?subject=USCM">Oskar</A> &#160;&#160;&#160;|&#160;&#160;&#160; Web Design: <A HREF="mailto:totte@nordstrom.ax?subject=USCM">Totte</A> &#160;&#160;&#160;|&#160;&#160;&#160; Server & Database: <A HREF="mailto:meradan+uscm@gmail.com?subject=USCM">Pontus</A></CENTER></SMALL>
+  <hr class="line">
+
+<SMALL><CENTER>Campaign & Content: <A HREF="mailto:oskar.grindemyr@gmail.com?subject=USCM">Oskar</A> | Web Design: <A HREF="mailto:totte@nordstrom.ax?subject=USCM">Totte</A> | Server & Database: <A HREF="mailto:meradan+uscm@gmail.com?subject=USCM">Pontus</A></CENTER></SMALL>
 <div style="font-size:smaller; text-align:center;">All content are copyright to their respective owners</div>
 </footer>
