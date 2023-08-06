dcfbe7a2a3fd5489ac60a642e2713574d908f8d4
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-08-06 15:15:40 +0200

    fix: html header, main & footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-</div>
-<BR><CENTER><IMG SRC="images/line.jpg" WIDTH="449" HEIGHT="1"></CENTER><BR>
-<SMALL><CENTER>Campaign & Content: <A HREF="mailto:oskar.grindemyr@gmail.com?subject=USCM">Oskar</A> &#160;&#160;&#160;|&#160;&#160;&#160; Web Production: <A HREF="mailto:tottemans@hotmail.com?subject=USCM">Totte</A> &#160;&#160;&#160;|&#160;&#160;&#160; Database Production: <A HREF="mailto:meradan+uscm@gmail.com?subject=USCM">Pontus</A></CENTER></SMALL>
+<footer class="footer">
+<CENTER><IMG SRC="images/line.jpg" WIDTH="449" HEIGHT="1"></CENTER><BR>
+<SMALL><CENTER>Campaign & Content: <A HREF="mailto:oskar.grindemyr@gmail.com?subject=USCM">Oskar</A> &#160;&#160;&#160;|&#160;&#160;&#160; Web Design: <A HREF="mailto:totte@nordstrom.ax?subject=USCM">Totte</A> &#160;&#160;&#160;|&#160;&#160;&#160; Server & Database: <A HREF="mailto:meradan+uscm@gmail.com?subject=USCM">Pontus</A></CENTER></SMALL>
 <div style="font-size:smaller; text-align:center;">All content are copyright to their respective owners</div>
-</DIV>
+</footer>
