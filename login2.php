90d2ac88a9bc7e7542d9017c82ec994192f37792
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-08-07 12:02:36 +0200

    feat: wrap login page with header & footer

--- login2.php
+++ login2.php
@@ -1,30 +1,19 @@
-<?php
-  session_start();
-  include("functions.php");
-?>
-<html>
-<head>
-<title>Login</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf8">
-<LINK REL="stylesheet" HREF="assets/style.css" TYPE="text/css"></LINK>
-</head>
-<body style="margin-top:40px">
+<h1 class="heading heading-h1">Log in</h1>
 
-<center>
-<div style="border: 1px solid #222222; width:300; height:115; overflow:hidden" >
-<form action="login.php?alt=login" method="post">
-      <table width="100%" border="0">
-        <tr>
-          <td width="36%" style="width:35%">Username</td>
-          <td width="64%" style="width:65%"><input type="text" name="anvandarnamn" style="width:100%"></td>
-        </tr>
-        <tr>
-          <td>Password</td>
-          <td><input type="password" name="losenord" style="width:100%"></td>
-        </tr>
-        <tr>
-          <td>Game:</td>
-          <td><select name="rpg" style="width:100%">
+<form class="form" action="login.php?alt=login" method="post">
+  <label for="anvandarnamn">
+    Username
+    <input type="text" id="anvandarnamn" name="anvandarnamn" autocomplete="username">
+  </label>
+
+  <label for="losenord">
+    Password
+    <input type="password" id="losenord" name="losenord" autocomplete="current-password">
+  </label>
+
+  <label for="rpg">
+    Game
+    <select id="rpg" name="rpg">
         <?php
         $db = getDatabaseConnection();
         $rpgsql="select rpg_name_short,table_prefix from RPG order by rpg_name";
@@ -36,22 +25,14 @@
              }
 
       ?>
-            </select></td>
-        </tr>
-        <tr>
-          <td>&nbsp;</td>
-          <td><div align="right"><font color="#CC0000">
-              <input name="submit" type="submit" value="Logga in">
-              </font> </div></td>
-        </tr>
-        <?php if(isset($_GET['error'])){?>
-        <tr>
-          <td colspan="2"><font color="#CC0000"><?php echo $_GET['error'];?></font></td>
-        </tr>
-        <?php } ?>
-      </table>
+    </select>
+  </label>
+
+  <input class="button" type="submit" value="Log in">
+
+  <?php if(isset($_GET['error'])){?>
+    <div class="colorfont">
+      <?php echo $_GET['error'];?>
+    </div>
+  <?php } ?>
 </form>
-</div>
-</center>
-</body>
-</html>
