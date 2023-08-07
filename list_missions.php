d2f088345b4d774023f5f99abb5158514b6c23c0
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-08-07 17:02:20 +0200

    feat(styling): update mission list

--- list_missions.php
+++ list_missions.php
@@ -1,9 +1,15 @@
 <h1 class="heading heading-h1">Missions</h1>
+<ul class="list">
 <?php
 $missionController = new MissionController();
 $missions = $missionController->getMissions();
 foreach ($missions as $mission) { ?>
-<a href="index.php?url=show_mission.php&id=<?php echo $mission->getId();?>"><?php
-  echo $mission->getPlatoonShortName();?>: <?php echo $mission->getShortName();?></a> <?php
-  echo $mission->getName();?><br><br>
+  <li>
+    <span class="tag tag-<?php echo strtolower($mission->getPlatoonShortName());?>"><?php echo $mission->getPlatoonShortName();?></span>
+    <a href="index.php?url=show_mission.php&id=<?php echo $mission->getId();?>">
+      <?php echo $mission->getShortName();?>
+    </a>
+    <?php echo $mission->getName();?>
+  </li>
 <?php } ?>
+</ul>
