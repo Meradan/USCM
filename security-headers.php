8bdc41726c0587049706bf206f0545b8445099c1
Kristoffer Nordström <totte.nordstrom@gmail.com>
2023-09-25 09:03:40 +0200

    fix(csp): form action for windows edge

--- security-headers.php
+++ security-headers.php
@@ -1,5 +1,5 @@
 <?php
-  header("Content-Security-Policy: default-src 'self'; connect-src 'self' https://hive.splitbee.io/; script-src 'self' 'unsafe-inline' https://cdn.splitbee.io/; style-src 'self' 'unsafe-inline' https://unpkg.com; font-src 'self'; img-src 'self' data:; media-src 'self'; frame-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; object-src 'none';");
+  header("Content-Security-Policy: default-src 'self'; connect-src 'self' https://hive.splitbee.io/; script-src 'self' 'unsafe-inline' https://cdn.splitbee.io/; style-src 'self' 'unsafe-inline' https://unpkg.com; font-src 'self'; img-src 'self' data:; media-src 'self'; frame-src 'self'; base-uri 'self'; form-action 'self' http://www.uscm.se; frame-ancestors 'none'; object-src 'none';");
   header("Permissions-Policy: accelerometer=(), autoplay=(), camera=(), display-capture=(), fullscreen=(), geolocation=(), gyroscope=(), microphone=(), payment=(), storage-access=(), web-share=(), xr-spatial-tracking=()");
   header("Referrer-Policy: strict-origin-when-cross-origin");
   header("Strict-Transport-Security: max-age=63072000; includeSubDomains; preload");
